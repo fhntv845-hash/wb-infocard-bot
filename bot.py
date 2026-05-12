75b3554d7585e1c902f42e952199c53bcfbac8a1
fhntv845 <fhntv845@gmail.com>
2026-05-12 15:24:56 +0300

    upgrade model

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ async def generate_text(message: Message):
 """
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-2.5-pro",
         contents=prompt
     )
 
